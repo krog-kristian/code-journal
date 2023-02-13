0e8dc47fe91d8635c12a8c3bd9a3c419247302cd
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-02-13 22:23:15 +0000

    Added event lister to form submit event and prevent default and capture data in an object with an entryID.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-var photoImage = document.querySelector('.journal-photo > img');
+var $photoImage = document.querySelector('.journal-photo > img');
 
-var inputURL = document.querySelector('#photo');
+var $inputURL = document.querySelector('#photo');
 
-inputURL.addEventListener('input', function () {
-  photoImage.setAttribute('src', event.target.value);
+$inputURL.addEventListener('input', function () {
+  $photoImage.setAttribute('src', event.target.value);
 });
+
+var $form = document.querySelector('form');
+
+$form.addEventListener('submit', save);
+
+function save(event) {
+  event.preventDefault();
+  var formInput = {};
+  formInput.title = $form.elements.title.value;
+  formInput.photo = $form.elements.photo.value;
+  formInput.notes = $form.elements.notes.value;
+  formInput.entryId = data.nextEntryId;
+}
