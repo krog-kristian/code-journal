37a0d49b08c012daf49587634d51916f897d4558
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-02-13 23:09:24 +0000

    Added data retrieval if code-journal data is present and parse it and reassigned to data object model.

--- js/data.js
+++ js/data.js
@@ -11,3 +11,8 @@ window.addEventListener('beforeunload', function (event) {
   var dataJSON = JSON.stringify(data);
   localStorage.setItem('code-journal', dataJSON);
 });
+
+if (localStorage.getItem('code-journal')) {
+  var previousData = localStorage.getItem('code-journal');
+  data = JSON.parse(previousData);
+}
