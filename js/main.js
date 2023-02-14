d683fdbbd4f30855bfe8284b92d9e927f0fc2282
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-02-14 17:34:29 +0000

    Created function to build dom tree for a single entry.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Data entry and storage.
 var $photoImage = document.querySelector('.journal-photo > img');
 
 var $inputURL = document.querySelector('#photo');
@@ -22,3 +23,28 @@ function save(event) {
   $photoImage.setAttribute('src', 'images/placeholder-image-square.jpg');
   $formJournal.reset();
 }
+
+// Viewing Entries
+
+function renderEntry(entry) {
+  var $listEntry = document.createElement('li');
+  $listEntry.setAttribute('class', 'row');
+  var $photoDiv = document.createElement('div');
+  var $image = document.createElement('img');
+  $photoDiv.setAttribute('class', 'column-half entry-photos');
+  $image.setAttribute('src', entry.photo);
+  $photoDiv.appendChild($image);
+  $listEntry.appendChild($photoDiv);
+  var $textDiv = document.createElement('div');
+  $textDiv.setAttribute('class', 'column-half');
+  var $entryTitle = document.createElement('h3');
+  $entryTitle.textContent = entry.title;
+  $textDiv.appendChild($entryTitle);
+  var $paragraph = document.createElement('p');
+  $paragraph.textContent = entry.notes;
+  $textDiv.appendChild($paragraph);
+  $listEntry.appendChild($textDiv);
+  return $listEntry;
+}
+
+renderEntry();
